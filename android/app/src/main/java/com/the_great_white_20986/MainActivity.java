44bb04fa425200398eb941adafc0301bf5531fe1
Crowdbotics <team@crowdbotics.com>
2020-10-02 18:39:51 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/the_great_white_20986/MainActivity.java
@@ -0,0 +1,15 @@
+package com.the_great_white_20986;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "the_great_white_20986";
+    }
+}
---
+++ ios/the_great_white_20986Tests/the_great_white_20986Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface the_great_white_20986 : XCTestCase
+
+@end
+
+@implementation the_great_white_20986
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
